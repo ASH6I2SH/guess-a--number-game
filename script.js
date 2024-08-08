827f0802068082d1eaf24f13bc24fa87083df15b
Ashish Pandey <ash6i2sh@gmail.com>
2024-08-09 01:11:14 +0530

    project finished though not deployed

--- script.js
+++ script.js
@@ -1,22 +1,86 @@
-const insert = document.getElementById('insert');
+let randomN = parseInt(Math.random() * 100 + 1);
 
-window.addEventListener('keydown', (e) => {
-  insert.innerHTML = `
-    <div class='color'>
-    <table>
-    <tr>
-      <th>Key</th>
-      <th>Keycode</th> 
-      <th>Code</th>
-    </tr>
-    <tr>
-      <td>${e.key === ' ' ? 'Space' : e.key}</td>
-      <td>${e.keyCode}</td> 
-      <td>${e.code}</td>
-    </tr>
+console.log(randomN);
 
-  </table>
-    </div>
-  `;
-});
+const button = document.querySelector(".button");
 
+const form = document.querySelector(".form");
+
+const guessField = document.querySelector(".guessField");
+
+const info = document.querySelector(".info");
+
+const numberOfGuessR = document.querySelector(".numberOfGuessR");
+
+const previousGuess = document.querySelector(".previousGuess");
+
+let playGame = true;
+
+let nG = 0;
+
+const elem = document.createElement("span");
+
+if (playGame) {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    let guess = parseInt(guessField.value);
+    checkGuess(guess);
+  });
+}
+
+function checkGuess(guess) {
+  if (guess === randomN) {
+    guessField.value = "";
+    elem.innerHTML = "<h2>You guessed the right no.</h2>";
+    info.appendChild(elem);
+    displayGuess(guess);
+    endGame();
+  }
+  else if(guess<randomN){
+        elem.innerHTML='<h2>guess is too low</h2>';
+        info.appendChild(elem);
+        nG++;
+        displayGuess(guess);
+  }
+
+
+  else{ 
+    elem.innerHTML='<h2>guess is too high</h2>';
+    info.appendChild(elem);
+    nG++;
+    displayGuess(guess);
+}
+}
+
+function displayGuess(guess) {
+guessField.value='';
+  document.querySelector(".previousGuess").innerHTML += ` ${guess}`;
+  numberOfGuessR.innerHTML = `${10 - nG}`;
+  if(parseInt(numberOfGuessR.innerHTML)===0){endGame();}
+}
+
+function endGame() {
+  guessField.setAttribute("disabled", "");
+  numberOfGuessR.innerHTML = "0";
+  playGame = false;
+  newGame();
+}
+
+function newGame() {
+  const btn = document.createElement("button");
+  btn.innerHTML = "start a new game";
+  info.appendChild(btn);
+  console.log(info);
+  btn.addEventListener("click", function (e) {
+    randomN = parseInt(Math.random() * 100 + 1);
+    console.log(randomN);
+    guessField.removeAttribute("disabled");
+    //previousGuess
+    document.querySelector(".previousGuess").innerHTML = "previous guess";
+    //numberOfGuessR
+    document.querySelector(".numberOfGuessR").innerHTML = "10";
+    info.removeChild(elem);
+    info.removeChild(btn);
+    playGame=true;
+  });
+}
